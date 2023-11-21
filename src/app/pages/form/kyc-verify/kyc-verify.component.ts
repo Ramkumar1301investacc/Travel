90319c8dbd40510ebf5c1fadbbd5bab7c742899f
Ramkumar1301investacc <ramkumar@investacc.co.in>
2023-11-21 15:27:54 +0530

    kyc form added

--- src/app/pages/form/kyc-verify/kyc-verify.component.ts
+++ src/app/pages/form/kyc-verify/kyc-verify.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-kyc-verify',
   templateUrl: './kyc-verify.component.html',
-  styleUrls: ['./kyc-verify.component.css']
+  styleUrls: ['./kyc-verify.component.css'],
 })
 export class KycVerifyComponent {
-
+  kycDetail = new FormGroup({
+    verificationOption: new FormControl(''),
+    CardNumber: new FormControl(''),
+    nomineeFirstName: new FormControl(''),
+    nomineeRelationship: new FormControl(''),
+    nomineeAge: new FormControl(''),
+    nomineeMail_id: new FormControl(''),
+    nomineePhone_no: new FormControl(''),
+  });
+  onSubmit() {
+    console.log(this.kycDetail.value)
+  }
 }
